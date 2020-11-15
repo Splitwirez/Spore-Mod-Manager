85791885288ba8012e27e86732b892180d7275fb
Splitwirez <Splitwirez@gmail.com>
2020-11-15 15:56:10 -0500

    Safeguarded updater to be able to fail without bringing the entire program down with it

--- SporeMods.CommonUI/Updater.cs
+++ SporeMods.CommonUI/Updater.cs
@@ -24,138 +24,170 @@ namespace SporeMods.CommonUI
 
         public static void CheckForUpdates(bool forceInstallUpdate)
         {
-            bool ignoreUpdates = Environment.GetCommandLineArgs().Contains(UpdaterService.IgnoreUpdatesArg);
-            if (!ignoreUpdates)
+            try
             {
-                if ((!forceInstallUpdate) && (Settings.UpdatingMode == Settings.UpdatingModeType.Disabled))
-                    return;
-
-                // Necessary to stablish SSL connection with Github API
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls |
-                    (SecurityProtocolType)768 | (SecurityProtocolType)3072;
-
-                // We will only show one error even if it cannot check the two updates
-                WebException webException = null;
-
-                UpdaterService.GithubRelease release = null;
-                bool hasProgramUpdate = false;
-                try
+                bool ignoreUpdates = Environment.GetCommandLineArgs().Contains(UpdaterService.IgnoreUpdatesArg);
+                if (!ignoreUpdates)
                 {
-                    hasProgramUpdate = UpdaterService.HasProgramUpdate(out release);
-                }
-                catch (WebException ex)
-                {
-                    webException = ex;
-                }
-                catch (Exception ex)
-                {
-                    MessageDisplay.ShowException(ex);
-                    return;
-                }
+                    if ((!forceInstallUpdate) && (Settings.UpdatingMode == Settings.UpdatingModeType.Disabled))
+                        return;
 
-                if (hasProgramUpdate)
-                {
-                    bool update = true;
-                    if ((!forceInstallUpdate) && (Settings.UpdatingMode == Settings.UpdatingModeType.AutoCheck))
+                    // Necessary to stablish SSL connection with Github API
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls |
+                        (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+
+                    // We will only show one error even if it cannot check the two updates
+                    WebException webException = null;
+
+                    UpdaterService.GithubRelease release = null;
+                    bool hasProgramUpdate = false;
+                    try
                     {
-                        update = MessageBox.Show(Settings.GetLanguageString("UpdateAvailableText"),
-                            Settings.GetLanguageString("UpdateAvailableTitle"), MessageBoxButton.YesNo) == MessageBoxResult.Yes;
+                        hasProgramUpdate = UpdaterService.HasProgramUpdate(out release);
+                    }
+                    catch (WebException ex)
+                    {
+                        webException = ex;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageDisplay.ShowException(ex);
+                        return;
                     }
 
-                    if (update)
-                    {
-                        string updaterPath = Path.Combine(Settings.TempFolderPath, "SporeModManagerSetup.exe");
-                        bool updateDownloadFinished = false;
-
-                        /*while ((!File.Exists(updaterPath)) || Permissions.IsFileLocked(updaterPath) || (!updateDownloadFinished))
-                        { }*/
-                        
-                        var progressDialog = GetProgressDialog(Settings.GetLanguageString("UpdatingProgressText"), (s, e) =>
-                        {
-                            /*Thread prgThread = new Thread(() =>
-                            {*/
-                                updateDownloadFinished = UpdaterService.UpdateProgram(release, (s_, e_) =>
-                                {
-                                    (s as BackgroundWorker).ReportProgress(e_.ProgressPercentage);
-                                });
-
-                            /*});
-                            prgThread.Start();*/
-                        });
-                        progressDialog.ShowDialog();
-
-                        if (progressDialog.Error != null)
-                        {
-                            MessageDisplay.ShowException(progressDialog.Error);
-                            return;
-                        }
-
-                        while (Permissions.IsFileLocked(updaterPath))
-                        { }
-                        Process.Start(updaterPath, "--update \"" + Path.GetDirectoryName(Process.GetCurrentProcess().GetExecutablePath()) + "\" \"" + Process.GetCurrentProcess().GetExecutablePath() + "\" --lang:" + Settings.CurrentLanguageCode);
-                        Process.GetCurrentProcess().Kill();
-                    }
-                }
-
-                //TODO remember to restore this
-                webException = null;
-
-                bool hasDllsUpdate = false;
-                try
-                {
-                    hasDllsUpdate = UpdaterService.HasDllsUpdate(out release);
-                }
-                catch (WebException ex)
-                {
-                    webException = ex;
-                }
-                catch (Exception ex)
-                {
-                    MessageDisplay.ShowException(ex);
-                    return;
-                }
-
-                if (webException != null)
-                {
-                    MessageBox.Show(Settings.GetLanguageString("Error_CannotCheckForUpdates") + "\n" + webException.ToString(), Settings.GetLanguageString("Error_CannotCheckForUpdatesTitle"));
-                    return;
-                }
-
-                if (hasDllsUpdate)
-                {
-                    // If we reach this point with a program update available, it means it didn't update
-                    // (as the update restarts the program), so we cannot continue
                     if (hasProgramUpdate)
                     {
-                        MessageBox.Show(Settings.GetLanguageString("Error_UpdateAvailableDlls"),
-                            Settings.GetLanguageString("Error_UpdateAvailableDllsTitle"));
-                    }
-                    else
-                    {
                         bool update = true;
-                        if (Settings.UpdatingMode == Settings.UpdatingModeType.AutoCheck)
+                        if ((!forceInstallUpdate) && (Settings.UpdatingMode == Settings.UpdatingModeType.AutoCheck))
                         {
-                            update = MessageBox.Show(Settings.GetLanguageString("UpdateAvailableDllsText"),
-                                Settings.GetLanguageString("UpdateAvailableDllsTitle"), MessageBoxButton.YesNo) == MessageBoxResult.Yes;
+                            update = MessageBox.Show(Settings.GetLanguageString("UpdateAvailableText"),
+                                Settings.GetLanguageString("UpdateAvailableTitle"), MessageBoxButton.YesNo) == MessageBoxResult.Yes;
                         }
 
                         if (update)
                         {
-                            var progressDialog = GetProgressDialog(Settings.GetLanguageString("UpdatingProgressDllsText"), (s, e) =>
+                            string updaterPath = Path.Combine(Settings.TempFolderPath, "SporeModManagerSetup.exe");
+                            bool updateDownloadFinished = false;
+
+                            /*while ((!File.Exists(updaterPath)) || Permissions.IsFileLocked(updaterPath) || (!updateDownloadFinished))
+                            { }*/
+
+                            var progressDialog = GetProgressDialog(Settings.GetLanguageString("UpdatingProgressText"), (s, e) =>
                             {
-                                UpdaterService.UpdateDlls(release, (s_, e_) =>
-                                {
-                                    (s as BackgroundWorker).ReportProgress(e_.ProgressPercentage);
-                                });
+                            /*Thread prgThread = new Thread(() =>
+                            {*/
+                                updateDownloadFinished = UpdaterService.UpdateProgram(release, (s_, e_) =>
+                            {
+                                        (s as BackgroundWorker).ReportProgress(e_.ProgressPercentage);
+                                    });
+
+                            /*});
+                            prgThread.Start();*/
                             });
                             progressDialog.ShowDialog();
 
                             if (progressDialog.Error != null)
                             {
                                 MessageDisplay.ShowException(progressDialog.Error);
+                                return;
+                            }
+
+                            while (Permissions.IsFileLocked(updaterPath))
+                            { }
+                            Process.Start(updaterPath, "--update \"" + Path.GetDirectoryName(Process.GetCurrentProcess().GetExecutablePath()) + "\" \"" + Process.GetCurrentProcess().GetExecutablePath() + "\" --lang:" + Settings.CurrentLanguageCode);
+                            Process.GetCurrentProcess().Kill();
+                        }
+                    }
+
+                    //TODO remember to restore this
+                    webException = null;
+
+                    bool hasDllsUpdate = false;
+                    try
+                    {
+                        hasDllsUpdate = UpdaterService.HasDllsUpdate(out release);
+                    }
+                    catch (WebException ex)
+                    {
+                        webException = ex;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageDisplay.ShowException(ex);
+                        return;
+                    }
+
+                    if (webException != null)
+                    {
+                        MessageBox.Show(Settings.GetLanguageString("Error_CannotCheckForUpdates") + "\n" + webException.ToString(), Settings.GetLanguageString("Error_CannotCheckForUpdatesTitle"));
+                        return;
+                    }
+
+                    if (hasDllsUpdate)
+                    {
+                        // If we reach this point with a program update available, it means it didn't update
+                        // (as the update restarts the program), so we cannot continue
+                        if (hasProgramUpdate)
+                        {
+                            MessageBox.Show(Settings.GetLanguageString("Error_UpdateAvailableDlls"),
+                                Settings.GetLanguageString("Error_UpdateAvailableDllsTitle"));
+                        }
+                        else
+                        {
+                            bool update = true;
+                            if (Settings.UpdatingMode == Settings.UpdatingModeType.AutoCheck)
+                            {
+                                update = MessageBox.Show(Settings.GetLanguageString("UpdateAvailableDllsText"),
+                                    Settings.GetLanguageString("UpdateAvailableDllsTitle"), MessageBoxButton.YesNo) == MessageBoxResult.Yes;
+                            }
+
+                            if (update)
+                            {
+                                var progressDialog = GetProgressDialog(Settings.GetLanguageString("UpdatingProgressDllsText"), (s, e) =>
+                                {
+                                    UpdaterService.UpdateDlls(release, (s_, e_) =>
+                                    {
+                                        (s as BackgroundWorker).ReportProgress(e_.ProgressPercentage);
+                                    });
+                                });
+                                progressDialog.ShowDialog();
+
+                                if (progressDialog.Error != null)
+                                {
+                                    MessageDisplay.ShowException(progressDialog.Error);
+                                }
                             }
                         }
                     }
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionNoExit(ex);
+            }
+        }
+
+        static bool exceptionShown = false;
+        static void ShowExceptionNoExit(Exception exception)
+        {
+            if (!exceptionShown)
+            {
+                exceptionShown = true;
+                Exception current = exception;
+                int count = 0;
+                string errorText = "\n\nPlease send the contents this MessageBox and all which follow it to rob55rod\\Splitwirez, along with a description of what you were doing at the time.\n\nThe Spore Mod Manager will exit after the last Inner exception has been reported.";
+                string errorTitle = "Something is very wrong here. Layer ";
+                while (current != null)
+                {
+                    MessageBox.Show(current.GetType() + ": " + current.Message + "\n" + current.Source + "\n" + current.StackTrace + errorText, errorTitle + count);
+                    count++;
+                    current = current.InnerException;
+                    if (count > 4)
+                        break;
+                }
+                if (current != null)
+                {
+                    MessageBox.Show(current.GetType() + ": " + current.Message + "\n" + current.Source + "\n" + current.StackTrace + errorText, errorTitle + count);
                 }
             }
         }
