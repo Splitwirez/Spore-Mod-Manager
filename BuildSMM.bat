a6c1f9527932e167c94a6192950e4239ca1124fd
Splitwirez <Splitwirez@gmail.com>
2021-03-25 21:48:19 -0400

    fix typo in BuildSMM.bat

--- BuildSMM.bat
+++ BuildSMM.bat
@@ -6,7 +6,8 @@ dotnet build .\SporeMods.Manager -c Release -p:PublishReadyToRun=true
 dotnet build .\SporeMods.KitImporter -c Release -p:PublishReadyToRun=true
 dotnet publish .\SporeMods.Setup -c Release --self-contained false -p:PublishSingleFile=true -p:PublishReadyToRun=true -r win-x86 -o .\bin\Updater
 dotnet publish .\SporeMods.Setup -c Release --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -r win-x86 -o .\bin\OfflineInstaller
-dotnet build .\SporeMods.KitUpgradeDownloader -c Release -p:PublishReadyToRun=true -o .\bin\LauncherKitUpgradeDownloaderpause
+dotnet build .\SporeMods.KitUpgradeDownloader -c Release -p:PublishReadyToRun=true -o .\bin\LauncherKitUpgradeDownloader
 robocopy ".\bin\Updater" ".\bin" *.exe
 robocopy ".\bin\OfflineInstaller" ".\bin" *.exe
-robocopy ".\bin\LauncherKitUpgradeDownloaderpause" ".\bin" *.exe
+robocopy ".\bin\LauncherKitUpgradeDownloader" ".\bin" *.exe
+pause
