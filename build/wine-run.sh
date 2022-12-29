cd1ecbba8eacf87ef12f488a771be25d8b671de2
Splitwirez <Splitwirez@gmail.com>
2022-12-29 13:22:55 -0500

    project config stuff

--- build/wine-run.sh
+++ build/wine-run.sh
@@ -1,3 +1,13 @@
-wineDebugPrefix=`cat ../../wineDebugPrefix.txt`
-echo Running in prefix \'$wineDebugPrefix\'.
-WINEPREFIX=$wineDebugPrefix wine "../devBin/Debug/Spore Mod Manager.exe"
+WINE_DEBUG_PREFIX=`cat ../../wineDebugPrefix.txt`
+echo Running in prefix \'$WINE_DEBUG_PREFIX\'.
+
+
+USER_BUILD_DIR="$HOME/.local/share/spore-mod-manager"
+ENABLE_USER_BUILD_DIR_PATH=`realpath "$USER_BUILD_DIR/BUILD_TO_USER_DIR"`
+if [ -f "$ENABLE_USER_BUILD_DIR_PATH" ]; then
+    BUILD_PATH="$USER_BUILD_DIR/bin"
+else
+    BUILD_PATH="../devBin"
+fi
+
+WINEPREFIX=$WINE_DEBUG_PREFIX wine "$BUILD_PATH/Debug/Spore Mod Manager.exe"
